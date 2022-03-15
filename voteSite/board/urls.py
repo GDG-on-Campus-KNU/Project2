06070891279a4b1b6783c70b233fe6d99bcf8564
seyoung8239 <seyoung8239@gmail.com>
2022-03-15 20:31:04 +0900

    Refactor: remake migrations as board model is changed

--- voteSite/board/urls.py
+++ voteSite/board/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BoardList, BoardDetail
 
 urlpatterns = [
-    path('boards/', BoardList.as_view()),
-    path('boards/<int:pk>/', BoardDetail.as_view()),
+    path('', BoardList.as_view()),
+    path('<int:pk>/', BoardDetail.as_view()),
 ]
