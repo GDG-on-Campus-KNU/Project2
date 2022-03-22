414bf9243aa7184d943d809afc192902f1fa041e
seyoung8239 <seyoung8239@gmail.com>
2022-03-23 04:39:29 +0900

    Feat: add JWT token

--- voteSite/auth_token/views.py
+++ voteSite/auth_token/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['GET'])
+def getRoutes(req):
+    routes = [
+        '/api/token',
+        '/api/token/refresh'
+    ]
+
+    return Response(routes)
