1c929eaab6192ac76b778e6f5b38e6be4b260345
waroad <waroad@knu.ac.kr>
2022-03-28 21:06:07 +0900

    pagination 기능 추가 settings.py에 pagination 기능 추가

--- voteSite/config/settings.py
+++ voteSite/config/settings.py
@@ -130,3 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20
+}
